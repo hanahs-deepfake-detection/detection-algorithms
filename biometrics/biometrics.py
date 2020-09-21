b8aa6a90dc835a26d76e52073e8e5cb79ca4e64f
Choi, jongyeop <69131265+chlwhdduq2357@users.noreply.github.com>
2020-09-21 14:30:53 +0900

    subtle change

--- biometrics/biometrics.py
+++ biometrics/biometrics.py
@@ -9,13 +9,13 @@ from keras.layers import Dense, LSTM
 def make_sq(inx):
     res=[]
     prog=0
-    for i in range(1): #3
+    for i in range(2): #3
         temp=[]
-        for j in range(10): #100
+        for j in range(3): #100
             prog+=1
             print(prog)
             coord=dataset.get_face_coords(inx,(i*10)+j+1, from_test_data=True)
-            temp.append(dataset.crop_frame(inx,(i*10)+j+1,coord))
+            temp.append(dataset.crop_frame(inx,(i*10)+j+1,coord, from_test_data=True))
         res.append(temp)
     return res
 
@@ -26,5 +26,7 @@ dataset=dataset_util.Dataset(FILE_PATH)
 
 file_list = os.listdir(FILE_PATH+"/test_videos")
 
-sq=make_sq(file_list[1])
+vid_data=make_sq(file_list[1])
 
+for sq in vid_data:
+    
