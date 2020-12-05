3141a4507b427bce9218eac959711a60aa10c7a1
Ryang Sohn <loop.infinitely@gmail.com>
2020-12-05 23:34:39 +0900

    Fix device placement

--- jitter-detection/model.py
+++ jitter-detection/model.py
@@ -27,52 +27,52 @@ dataset_dir = sys.argv[1]
 batch_size = int(sys.argv[2])
 video_frames = int(sys.argv[3])
 
-inputs = keras.Input((video_frames, 384, 512, 3), batch_size=batch_size)
-x = TimeDistributed(Conv2D(32, kernel_size=(3, 3), activation='relu'))(inputs)
-x = TimeDistributed(MaxPool2D())(x)
-x = TimeDistributed(Conv2D(32, kernel_size=(3, 3), activation='relu'))(x)
-x = TimeDistributed(MaxPool2D())(x)
-x = TimeDistributed(Flatten())(x)
-x = TimeDistributed(Dense(64, activation='tanh', kernel_initializer='zeros'))(x)
-x = TimeDistributed(Dropout(0.5))(x)
-x = TimeDistributed(Dense(6, activation='tanh', kernel_initializer='zeros',
-                    bias_initializer=lambda shape, dtype=None: tf.constant(
-                        [1, 0, 0, 0, 1, 0], tf.float32
-                    )))(x)
-x = Lambda(lambda ls: tf.concat([ls[0], tf.reshape(ls[1],
-           (batch_size, video_frames, -1))], -1))([x, inputs])
-x = TimeDistributed(BilinearSampler(input_shape=(batch_size, 384, 512, 3),
-                    output_shape=(batch_size, 224, 224, 3)))(x)
-resnet = ResNet101V2(include_top=False, weights=None)
-x = TimeDistributed(resnet)(x)
-x = TimeDistributed(Flatten())(x)
-x = LSTM(32, return_sequences=True)(x)
-x = LSTM(32)(x)
-x = Dense(10, activation='relu')(x)
-x = BatchNormalization()(x)
-x = Dense(1, activation='sigmoid')(x)
-model = keras.Model(inputs=inputs, outputs=x)
-model.compile(loss='mse', optimizer='rmsprop', metrics=['accuracy'])
-model.summary()
-
-dataset = dataset_util.Dataset(dataset_dir, 'faceforensics')
-dataset_df = dataset.get_metadata_dataframe()
-filenames = dataset_df.index.to_list()
-labels = dataset_df['label'].to_dict()
-file_set = set(filenames)
-train_files = sample(filenames, int(sys.argv[4]))
-train_file_set = set(train_files)
-valid_file_set = file_set - train_file_set
-valid_files = list(valid_file_set)
-train_data_gen = VideoPipeline(train_files, labels, batch_size, video_frames)
-valid_data_gen = VideoPipeline(valid_files, labels, batch_size, video_frames)
-
-checkpoint_callback = keras.callbacks.ModelCheckpoint('best_model',
-                                                      monitor='loss',
-                                                      save_weights_only=True,
-                                                      mode='auto',
-                                                      verbose=1)
-tensorboard_callback = keras.callbacks.TensorBoard(log_dir='./logs')
 with tf.device(sys.argv[5]):
+    inputs = keras.Input((video_frames, 384, 512, 3), batch_size=batch_size)
+    x = TimeDistributed(Conv2D(32, kernel_size=(3, 3), activation='relu'))(inputs)
+    x = TimeDistributed(MaxPool2D())(x)
+    x = TimeDistributed(Conv2D(32, kernel_size=(3, 3), activation='relu'))(x)
+    x = TimeDistributed(MaxPool2D())(x)
+    x = TimeDistributed(Flatten())(x)
+    x = TimeDistributed(Dense(64, activation='tanh', kernel_initializer='zeros'))(x)
+    x = TimeDistributed(Dropout(0.5))(x)
+    x = TimeDistributed(Dense(6, activation='tanh', kernel_initializer='zeros',
+                        bias_initializer=lambda shape, dtype=None: tf.constant(
+                            [1, 0, 0, 0, 1, 0], tf.float32
+                        )))(x)
+    x = Lambda(lambda ls: tf.concat([ls[0], tf.reshape(ls[1],
+               (batch_size, video_frames, -1))], -1))([x, inputs])
+    x = TimeDistributed(BilinearSampler(input_shape=(batch_size, 384, 512, 3),
+                        output_shape=(batch_size, 224, 224, 3)))(x)
+    resnet = ResNet101V2(include_top=False, weights=None)
+    x = TimeDistributed(resnet)(x)
+    x = TimeDistributed(Flatten())(x)
+    x = LSTM(32, return_sequences=True)(x)
+    x = LSTM(32)(x)
+    x = Dense(10, activation='relu')(x)
+    x = BatchNormalization()(x)
+    x = Dense(1, activation='sigmoid')(x)
+    model = keras.Model(inputs=inputs, outputs=x)
+    model.compile(loss='mse', optimizer='rmsprop', metrics=['accuracy'])
+    model.summary()
+
+    dataset = dataset_util.Dataset(dataset_dir, 'faceforensics')
+    dataset_df = dataset.get_metadata_dataframe()
+    filenames = dataset_df.index.to_list()
+    labels = dataset_df['label'].to_dict()
+    file_set = set(filenames)
+    train_files = sample(filenames, int(sys.argv[4]))
+    train_file_set = set(train_files)
+    valid_file_set = file_set - train_file_set
+    valid_files = list(valid_file_set)
+    train_data_gen = VideoPipeline(train_files, labels, batch_size, video_frames)
+    valid_data_gen = VideoPipeline(valid_files, labels, batch_size, video_frames)
+
+    checkpoint_callback = keras.callbacks.ModelCheckpoint('best_model',
+                                                          monitor='loss',
+                                                          save_weights_only=True,
+                                                          mode='auto',
+                                                          verbose=1)
+    tensorboard_callback = keras.callbacks.TensorBoard(log_dir='./logs')
     model.fit(train_data_gen, epochs=100, validation_data=valid_data_gen,
               callbacks=[checkpoint_callback, tensorboard_callback])
